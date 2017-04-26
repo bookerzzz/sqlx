341cd5c9be1f0a92a3275d65a184b82a73ec30c7
gm42 <gm42@users.noreply.github.com>
2017-04-26 18:23:14 +0200

    Store only a section of the stacktrace

--- track_tx.go
+++ track_tx.go
@@ -3,20 +3,34 @@ package sqlx
 import (
 	"database/sql"
 	"runtime"
+	"strings"
 	"sync"
 )
 
 var (
 	trackedStartedTransactions = map[*sql.Tx]string{}
 	trackedTransactionsMutex   sync.Mutex
-	stackTraceBuf              [1 << 16]byte
+	stackTraceBuf              [4096]byte
 )
 
 func trackStartTx(tx *sql.Tx) {
 	trackedTransactionsMutex.Lock()
 
 	n := runtime.Stack(stackTraceBuf[:], true)
-	trackedStartedTransactions[tx] = string(stackTraceBuf[:n])
+
+	s := string(stackTraceBuf[:n])
+	p := strings.Index(s, "\n\n")
+	if p != -1 {
+		s = s[:p]
+	}
+
+	lines := strings.Split(s, "\n")
+	if len(lines) > 5 {
+		lines = lines[5:]
+		s = strings.Join(lines, "\n")
+	}
+
+	trackedStartedTransactions[tx] = s
 
 	trackedTransactionsMutex.Unlock()
 }
